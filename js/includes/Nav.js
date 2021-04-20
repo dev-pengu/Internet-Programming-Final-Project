ab4820aafa64ed380b174e4168dbabf88355e266
Matt T Lippelman <lippelman515@live.missouristate.edu>
2021-04-19 22:47:20 -0500

    Finished up contact page and adjusted music page to account for change in reusable elements

--- js/includes/Nav.js
+++ js/includes/Nav.js
@@ -17,7 +17,7 @@ class Nav extends HTMLElement {
         <div id="sidenav" class ="sidenav">
             <a href="javascript:void(0)" class="close-btn" onclick="closeNav()">&times;</a>
             <a href="index.html">Home</a>
-            <a href="#">Music Resources</a>
+            <a href="music.html">Music Resources</a>
             <a href="#">Image Resources</a>
             <a href="#">SFX Resources</a>
             <a href="#">Video Resources</a>
