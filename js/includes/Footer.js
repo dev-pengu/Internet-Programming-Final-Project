49fc29a48e7146184763126058150f63c116e95f
Matt T Lippelman <lippelman515@live.missouristate.edu>
2021-04-20 00:09:17 -0500

    Made changes to layout css to keep from sizing too low

--- js/includes/Footer.js
+++ js/includes/Footer.js
@@ -1,3 +1,5 @@
+
+
 class Footer extends HTMLElement {
     constructor() {
         super();
@@ -7,7 +9,7 @@ class Footer extends HTMLElement {
         this.innerHTML = `
         <div id="page-footer">
             <div id="attribution">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-            <small id="copyright">&copy; <script>document.write(getCurrentYear());</script> Matt Lippelman & Zach Reynolds</small>
+            <small id="copyright">&copy;` + getCurrentYear() + ` Matt Lippelman & Zach Reynolds</small>
         </div>
         `
     }
